48bebd11ec6a791127997642d57b2bfeee181241
mramesh429 <rameshb4all@gmail.com>
2019-05-17 07:46:39 +0530

    First commit....

---
+++ src/Test1.java
@@ -0,0 +1,19 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry ="Yes";
+		
+		if(hungry=="Yes")
+		{
+			System.out.println("eat somthing...");
+		}
+		else
+		{
+			System.out.println("Do work......");
+		}
+
+	}
+
+}
